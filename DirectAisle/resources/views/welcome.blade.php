ab5dea673764f03ae74a188abcc4496f89b59273
kflores88 <kflores02@aurora.edu>
2017-04-12 14:09:18 -0500

    Auth
    Login and Register

--- DirectAisle/resources/views/welcome.blade.php
+++ DirectAisle/resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    CODELaravel
                 </div>
 
                 <div class="links">
